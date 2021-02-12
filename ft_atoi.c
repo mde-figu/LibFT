64b16f8fa592bdca5f0ac9b72ddfb413c6547e27
mde-figu <markusfwirz@gmail.com>
2021-02-12 19:15:22 -0300

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 19:23:25 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/12 18:31:18 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/12 19:14:55 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,19 @@ int		ft_atoi(const char *nptr)
 	sign = 1;
 	count = 0;
 	countstr = 0;
-	while ((nptr[count] >= '\t' && nptr[count] <= '\r') || (nptr[count] == 32))
+	while (nptr[count] == '\t' || nptr[count] == '\r' || nptr[count] == '\v'
+            nptr[count] == '\f' || nptr[count] == '\n' || nptr[count] == ' ')
 		count = count + 1;
-	while (nptr[count] == '-' || nptr[count] == '+')
+	if (nptr[count] == '-' || nptr[count] == '+')
 	{
 		if (nptr[count] == '-')
 			sign = sign * (-1);
 		count = count + 1;
 	}
-	if (nptr[count] < 48 && nptr[count] > 57)
-		return (countstr);
-	while (nptr[count] >= 48 && nptr[count] <= 57)
+	while (nptr[count] >= 48 && nptr[count] >= 57)
 	{
-		countstr = (countstr * 10) + (nptr[count] - '0');
-		count = count + 1;
-	}
-	countstr = countstr * sign;
-	return (countstr);
+    	countstr = (countstr * 10) + (nptr[count] - '0');
+        count = count + 1;
+    }
+	return (countstr * sign);
 }
