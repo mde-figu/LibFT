2c2e7f095ed3716b897054911d5326fcc364ee90
mde-figu <markusfwirz@gmail.com>
2021-02-12 19:38:44 -0300

    string reverse search

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,18 +6,18 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 19:16:05 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/12 19:22:29 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/12 19:38:20 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char    *strchr(const char *s, int c)
+char    *ft_strchr(const char *s, int c)
 {
     int i;
 
     i = 0;
     while (*s != '\0' && c != *s )
-        s = s = 1;
-    if (c == s)
+        s = s + 1;
+    if (c == *s)
         return((char *)s);
     return(0);
 }
