eef217712ea975179a9160bdb97d7604f5936c72
mde-figu <markusfwirz@gmail.com>
2021-02-14 17:24:15 -0300

    calloc feito

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mirkios <mirkios@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 12:25:42 by mirkios           #+#    #+#             */
-/*   Updated: 2021/02/14 12:31:06 by mirkios          ###   ########.fr       */
+/*   Updated: 2021/02/14 17:23:45 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@ void        *ft_calloc(size_t nmemb, size_t size);
     size_t i;
 
     i = nmemb * size;
-
-    return(ptr);
+    if (nmemb == NULL || size == NULL)
+        return (0);
+    else
+    {
+        ptr = (i);
+        ft_bzero(ptr, i);
+        return (ptr);
+    }
 }
