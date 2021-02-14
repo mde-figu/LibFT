f2ef7f5bb25ed903f75f83a0a9efb4f053c9dd6d
Mirkios <markusfwirz@gmail.com>
2021-02-14 12:24:50 -0300

    ft_strncmp feito

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mirkios <mirkios@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 20:36:34 by mirkios           #+#    #+#             */
-/*   Updated: 2021/02/14 12:11:14 by mirkios          ###   ########.fr       */
+/*   Updated: 2021/02/14 12:22:36 by mirkios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,13 @@ int     ft_strncmp(const char *s1, const char *s2, size_t n)
     {
         if (s1[i] != s2[i])
         {
+            if (s1[i] == '\0' && s2[i] == '\0')
+                return (0)
             if (s1[i] == '\0')
-                return (s2[i] - s1[i])
+                return ((s2[i]) * (-1))
             if (s2[i] == '\0')
-                return (s1[i] - s2[i])
+                return (s1[i])
+            return (s1[i] - s2[i])
         }
         i = i + 1;
     }
