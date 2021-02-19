50fdf60dda74dbefef4657df8a647e6d6a4d22ec
mde-figu <markusfwirz@gmail.com>
2021-02-19 15:16:51 -0300

    make3

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,24 +6,24 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 12:25:42 by mirkios           #+#    #+#             */
-/*   Updated: 2021/02/19 00:45:59 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/19 15:16:46 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void        *ft_calloc(size_t nmemb, size_t size)
+void		*ft_calloc(size_t nmemb, size_t size)
 {
-    void *ptr;
-    size_t i;
+	void *ptr;
+	size_t i;
 
-    i = nmemb * size;
-    if (nmemb == 0 || size == 0)
-        return (0);
-    else
-    {
-        ptr = malloc(i);
-        ft_bzero(ptr, i);
-        return (ptr);
-    }
+	i = nmemb * size;
+	if (nmemb == 0 || size == 0)
+		return (0);
+	else
+	{
+		ptr = malloc(i);
+		ft_bzero(ptr, i);
+		return (ptr);
+	}
 }
--- ft_substr.c
+++ ft_substr.c
@@ -6,30 +6,30 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 13:42:03 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/19 00:41:14 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/19 15:16:16 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char        *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    char    *nstr;
-    size_t  i;
-    size_t  j;
+	char	*nstr;
+	size_t  i;
+	size_t  j;
 
-    nstr = ((char *)malloc(len + 1));
-    if (s == NULL || nstr == NULL)
-        return (0);
-    i = start;
-    j = 0;
-    while (i < ft_strlen(s) && j < len)
-    {
-        nstr[j] = s[i];
-        j = j + 1;
-        i = i + 1;
-    }
-    nstr[j] = '\0';
-    return(nstr);
+	nstr = ((char *)malloc(len + 1));
+	if (s == NULL || nstr == NULL)
+		return (0);
+	i = start;
+	j = 0;
+	while (i < ft_strlen(s) && j < len)
+	{
+		nstr[j] = s[i];
+		j = j + 1;
+		i = i + 1;
+	}
+	nstr[j] = '\0';
+	return(nstr);
 
 }
