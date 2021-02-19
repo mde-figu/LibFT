212279a8a89b8565c8c75a54195adafe9c643e09
mde-figu <markusfwirz@gmail.com>
2021-02-19 18:22:56 -0300

    putstr_fd

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,13 +6,13 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 20:12:42 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/19 01:11:30 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/19 18:22:26 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		ft_putstr(char *str, int fd)
+void		ft_putstr_fd(char *str, int fd)
 {
 	int counter;
 
