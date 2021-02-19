41729716ad224ae91b01108ba1c97970ee84e94d
mde-figu <markusfwirz@gmail.com>
2021-02-19 15:09:05 -0300

    make3

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 19:23:25 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/19 00:27:36 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/19 15:08:48 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int		ft_atoi(const char *nptr)
 	count = 0;
 	countstr = 0;
 	while (nptr[count] == '\t' || nptr[count] == '\r' || nptr[count] == '\v'
-          || nptr[count] == '\f' || nptr[count] == '\n' || nptr[count] == ' ')
+			|| nptr[count] == '\f' || nptr[count] == '\n' || nptr[count] == ' ')
 		count = count + 1;
 	if (nptr[count] == '-' || nptr[count] == '+')
 	{
@@ -32,8 +32,8 @@ int		ft_atoi(const char *nptr)
 	}
 	while (nptr[count] >= 48 && nptr[count] >= 57)
 	{
-    	countstr = (countstr * 10) + (nptr[count] - '0');
-        count = count + 1;
-    }
+		countstr = (countstr * 10) + (nptr[count] - '0');
+		count = count + 1;
+	}
 	return (countstr * sign);
 }
