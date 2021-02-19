1c443fbcc0fad0dae6bf57bab5790fb798c1c67e
mde-figu <markusfwirz@gmail.com>
2021-02-19 20:25:11 -0300

    strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,23 +6,20 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 13:16:05 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/19 16:01:07 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/19 20:24:53 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char		*strdup(const char *s)
+char	*ft_strdup(const char *s)
 {
-	char	*ret;
+	int		len;
+	char	*dest;
 
-	ret = ((char *)malloc(ft_strlen(s) + 1));
-	if (ret == NULL || ft_strlen(s) == '\0')
-		return (0);
-	else
-	{
-		ret = malloc(0);
-		ft_memcpy(ret, s, ft_strlen(s) + 1);
-		return (ret);
-	}
+	len = ft_strlen(s);
+	if (!(dest = (char*)malloc((len + 1) * sizeof(char))))
+		return (NULL);
+	ft_strlcpy(dest, s, (len + 1));
+	return (dest);
 }
