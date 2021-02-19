d1d71b2f997b9c15d7bf0e5d7820f95c1c70cc0f
mde-figu <markusfwirz@gmail.com>
2021-02-19 16:34:44 -0300

    norma3

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 12:23:34 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/19 16:33:50 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/19 16:34:40 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 int		ft_isalnum(int c)
 {
-int		ret;
+	int	ret;
 
-ret = 1;
-if ((c >= 65 && c <= 90) ||
-	(c >= 97 && c <= 122))
-	{
-		ret = 1;
-	}
-else if (c >= 48 && c <= 57)
-	{
-		ret = 1;
-	}
-else
-	{
-		ret = 0;
-	}
-return (ret);
+	ret = 1;
+	if ((c >= 65 && c <= 90) ||
+		(c >= 97 && c <= 122))
+		{
+			ret = 1;
+		}
+	else if (c >= 48 && c <= 57)
+		{
+			ret = 1;
+		}
+	else
+		{
+			ret = 0;
+		}
+	return (ret);
 }
