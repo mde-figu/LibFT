6b13dd94aa854486ded459a1d98cd5d7a3a683ee
mde-figu <markusfwirz@gmail.com>
2021-02-19 15:53:51 -0300

    make4

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 20:23:28 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/19 15:44:38 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/19 15:53:47 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,29 @@
 
 void		ft_putnbr_fd(int n, int fd)
 {
-long int 	count;
+long int	count;
 
 count = 0;
-	if (n == -2147483648)
-		write(1, "-2147483648", 11);
+if (n == -2147483648)
+	write(1, "-2147483648", 11);
+else
+{
+	if (n < 0 && count == 0)
+	{
+		count = 0;
+		ft_putchar_fd('-', fd);
+		n = (n * (-1));
+		ft_putnbr_fd(n, fd);
+		count = count + 1;
+	}
+	else if (n > 9)
+	{
+		ft_putnbr_fd((n / 10), fd);
+		ft_putnbr_fd((n % 10), fd);
+	}
 	else
 	{
-		if (n < 0 && count == 0)
-		{
-			count = 0;
-			ft_putchar_fd('-', fd);
-			n = (n * (-1));
-			ft_putnbr_fd(n, fd);
-			count = count + 1;
-		}
-		else if (n > 9)
-		{
-			ft_putnbr_fd((n / 10), fd);
-			ft_putnbr_fd((n % 10), fd);
-		}
-		else
-		{
-			ft_putchar_fd(n + '0', fd);
-		}
+		ft_putchar_fd(n + '0', fd);
 	}
+}
 }
