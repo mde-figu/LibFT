645a26d64b14aa4a454ad88df3aa5a673197b23d
mde-figu <markusfwirz@gmail.com>
2021-02-19 15:31:18 -0300

    make4

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 20:23:28 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/19 00:48:31 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/19 15:31:13 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void		ft_putnbr_fd(int n, int fd)
 {
 long int count;
+
 count = 0;
 	if (n == -2147483648)
 		write(1, "-2147483648", 11);
--- ft_strchr.c
+++ ft_strchr.c
@@ -6,17 +6,17 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 19:16:05 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/18 17:29:00 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/19 15:30:31 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strchr(const char *s, int c)
+char		*ft_strchr(const char *s, int c)
 {
-    while (*s != '\0' && c != *s)
-        s = s + 1;
-    if (c == *s)
-        return((char *)s);
-    return(0);
+	while (*s != '\0' && c != *s)
+		s = s + 1;
+	if (c == *s)
+		return((char *)s);
+	return(0);
 }
