b73abce4351ce6102ef73e55e8b1a9fd9fed3651
mde-figu <markusfwirz@gmail.com>
2021-02-19 19:38:06 -0300

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 20:36:34 by mirkios           #+#    #+#             */
-/*   Updated: 2021/02/19 15:11:47 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/19 19:37:41 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t i;
+	unsigned char c;
+	unsigned char d;
 
-	i = 0;
 	if (n == 0)
 		return (0);
-	while (i != n)
+	n = n - 1;
+	while (n--)
 	{
-		if (s1[i] != s2[i])
-		{
-			if (s1[i] == '\0' && s2[i] == '\0')
-				return (0);
-			if (s1[i] == '\0')
-				return ((s2[i]) * (-1));
-			if (s2[i] == '\0')
-				return (s1[i]);
-			return (s1[i] - s2[i]);
-		}
-		i = i + 1;
+		c = (unsigned char)*s1;
+		s1 = s1 + 1;
+		s = (unsigned char)*s2;
+		s2 = s2 + 1;
+		if (c == '\0' || c != d)
+			return (c - d);
 	}
-	return (0);
+	c = (unsigned char)*s1;
+	s1 = s1 + 1;
+	d = (unsigned char)*s2;
+	s2 = s2 + 1;
+	return (c - d);
 }
