749ad7840cf3d96f35e6203b7cbdc93b02ddf1b5
mde-figu <markusfwirz@gmail.com>
2021-02-19 20:14:03 -0300

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 13:50:34 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/19 20:12:52 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/19 20:13:59 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstlen)
 	size_t i;
 	size_t j;
 
-	if (size <= ft_strlen(dst))
-		return (ft_strlen(src) + size);
+	if (dstlen <= ft_strlen(dst))
+		return (ft_strlen(src) + dstlen);
 	i = 0;
 	while (dst[i] != '\0')
 	{
 		i++;
 	}
 	j = 0;
-	while (i + j < size - 1 && src[j] != '\0')
+	while (i + j < dstlen - 1 && src[j] != '\0')
 	{
 		dst[i + j] = src[j];
 		j++;
