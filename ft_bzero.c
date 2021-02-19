0f40aba614ef950374090e2c71e4f80afaf2496f
mde-figu <markusfwirz@gmail.com>
2021-02-19 16:37:48 -0300

    norma4

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 16:04:39 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/19 15:13:19 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/19 16:37:41 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 void		ft_bzero(void *s, size_t n)
 {
 	if (sizeof(s) >= 0)
-		ft_memset(s, '\0' , n);
+		ft_memset(s, '\0', n);
 }
