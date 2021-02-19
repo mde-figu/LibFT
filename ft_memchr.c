3dbfe2a23c3ba5afebe37f6332dabfa4fe824b48
mde-figu <markusfwirz@gmail.com>
2021-02-19 16:21:43 -0300

    norma1

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 11:02:11 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/19 16:13:02 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/19 16:21:23 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void		*ft_memchr(const void *s, int c, size_t len)
 {
 	unsigned char	*p;
-	unsigned char   *fdchar;
+	unsigned char	*fdchar;
 
 	p = (unsigned char*)s;
 	fdchar = '\0';
@@ -28,7 +28,7 @@ void		*ft_memchr(const void *s, int c, size_t len)
 		else
 		{
 			fdchar = p;
-			break;
+			break ;
 		}
 	}
 	return (fdchar);
