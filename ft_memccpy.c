39ab93ad398f62b2a5f7a14e48c1e5a97deb7db3
mde-figu <markusfwirz@gmail.com>
2021-02-19 19:02:40 -0300

    memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,19 +6,25 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 12:16:43 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/19 16:27:46 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/19 19:02:24 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memccpy(void *dest, const void *src, int c, size_t len)
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
-	void	*p;
+	int	i;
 
-	p = ft_memchr(src, c, len);
-	if (p != NULL)
-		return (ft_memcpy(dest, src, p - src + 1));
-	ft_memcpy(dest, src, len);
+	i = 0;
+	while (i < (int)n)
+	{
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		if (((unsigned char *)src)[i] == (unsigned char)c)
+		{
+			return ((void *)(dest + i + 1));
+		}
+		i++;
+	}
 	return (NULL);
 }
