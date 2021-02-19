2980a34a17650b3fb42429f4f77a7e62e5e13ec6
mde-figu <markusfwirz@gmail.com>
2021-02-19 16:26:43 -0300

    norma2

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 12:16:43 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/19 16:25:12 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/19 16:26:30 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 void	*ft_memccpy(void *dest, const void *src, int c, size_t len)
 {
-	void *p = ft_memchr(src, c, len);
-
+	void	*p;
+	
+	p = ft_memchr(src, c, len);
 	if (p != NULL)
 		return (ft_memcpy(dest, src, p - src + 1));
 	ft_memcpy (dest, src, len);
