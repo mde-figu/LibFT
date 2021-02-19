4e7fcc9041afa637b03ba5df63ef5be676d5bc62
mde-figu <markusfwirz@gmail.com>
2021-02-19 20:22:55 -0300

    putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,37 +6,83 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 20:23:28 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/19 15:56:06 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/19 20:22:42 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		ft_putnbr_fd(int n, int fd)
+static	void	rev_array(char *str)
 {
-	long int	count;
+	int		len;
+	int		i;
+	char	c;
 
-	count = 0;
-	if (n == -2147483648)
-		write(1, "-2147483648", 11);
+	len = 0;
+	i = 0;
+	while (str[len] != '\0')
+		len++;
+	while (i < len / 2)
+	{
+		c = str[i];
+		str[i] = str[len - i - 1];
+		str[len - i - 1] = c;
+		i++;
+	}
+}
+
+static	int		check_neg(int nb)
+{
+	if (nb < 0)
+		return (1);
+	else
+		return (0);
+}
+
+static	void	nb_to_char(char *str, unsigned int nb, int neg)
+{
+	int i;
+
+	i = 0;
+	while (nb > 0)
+	{
+		str[i] = (nb % 10) + '0';
+		nb /= 10;
+		i++;
+	}
+	str[i] = (nb % 10) + '0';
+	if (neg)
+	{
+		str[i] = '-';
+		str[i + 1] = '\0';
+	}
+	else
+		str[i] = '\0';
+}
+
+void			ft_putnbr_fd(int n, int fd)
+{
+	char			str[12];
+	int				neg;
+	int				i;
+	unsigned int	nb;
+
+	neg = check_neg(n);
+	i = 0;
+	if (neg)
+		nb = n * (-1);
+	else
+		nb = n;
+	if (n == 0)
+		ft_putchar_fd('0', fd);
 	else
 	{
-		if (n < 0 && count == 0)
-		{
-			count = 0;
-			ft_putchar_fd('-', fd);
-			n = (n * (-1));
-			ft_putnbr_fd(n, fd);
-			count = count + 1;
-		}
-		else if (n > 9)
-		{
-			ft_putnbr_fd((n / 10), fd);
-			ft_putnbr_fd((n % 10), fd);
-		}
-		else
-		{
-			ft_putchar_fd(n + '0', fd);
-		}
+		nb_to_char(str, nb, neg);
+		rev_array(str);
+	}
+	while (str[i] != '\0')
+	{
+		ft_putchar_fd(str[i], fd);
+		i++;
 	}
 }
