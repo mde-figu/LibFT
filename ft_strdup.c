a3a21561b7640bb6154896558bc44cf48fe4d5ed
mde-figu <markusfwirz@gmail.com>
2021-02-19 16:00:31 -0300

    make4

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 13:16:05 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/19 15:28:47 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/19 16:00:25 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 char		*strdup(const char *s)
 {
-char *ret;
+	char	*ret;
 
-ret = ((char *)malloc(ft_strlen(s) + 1));
-if (ret == NULL || ft_strlen(s) == '\0')
-	return (0);
-else
-{
-	ret = malloc(0);
-	ft_memcpy(ret, s, ft_strlen(s) + 1);
-	return (ret);
-}
+	ret = ((char *)malloc(ft_strlen(s) + 1));
+	if (ret == NULL || ft_strlen(s) == '\0')
+		return (0);
+	else
+	{
+		ret = malloc(0);
+		ft_memcpy(ret, s, ft_strlen(s) + 1);
+		return (ret);
+	}
 
 }
