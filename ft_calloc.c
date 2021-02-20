a8e5df94fcaeed88f1e7413bb1a7da8a55090a17
Mirkios <markusfwirz@gmail.com>
2021-02-20 17:48:11 -0300

    malloc safe

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mirkios <mirkios@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 12:25:42 by mirkios           #+#    #+#             */
-/*   Updated: 2021/02/20 17:45:47 by mirkios          ###   ########.fr       */
+/*   Updated: 2021/02/20 17:48:04 by mirkios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,14 @@ void		*ft_calloc(size_t nmemb, size_t size)
 	size_t	i;
 
 	i = nmemb * size;
-	if (nmemb == 0 || size == 0)
+	if (nmemb == 0)
 		return (0);
 	else
 	{
 		ptr = malloc(i);
 		if (!ptr)
 			return (NULL);
-		else if (ptr == NULL)
+		else if (size == NULL)
 			return (ptr);
 		ft_bzero(ptr, i);
 		return (ptr);
