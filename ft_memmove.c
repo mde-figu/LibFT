1abe92b7b15a34e13c27da2261acf60f5708ad63
Mirkios <markusfwirz@gmail.com>
2021-02-20 13:55:56 -0300

    memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,32 +6,23 @@
 /*   By: mirkios <mirkios@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 10:41:19 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/20 12:13:05 by mirkios          ###   ########.fr       */
+/*   Updated: 2021/02/20 13:55:42 by mirkios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	size_t	i;
-	char			temp[n + 1];
+	unsigned char *d;
+	unsigned char *s;
 
-	i = 0;
-	temp[0] = 0;
-	if(dest && src)
-	{
-		while (i < n)
-		{
-			temp[i] = *((char *)src + i);
-			i++;
-		}
-		i = 0;
-		while (i < n)
-		{
-			*((char *)dest + i) = temp[i];
-			i++;
-		}
-	}
-	return (dest);
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	if (d < s)
+		return (ft_memcpy(dst, src, len));
+	else
+		while (len-- && dst != src)
+			d[len] = s[len];
+	return (dst);
 }
