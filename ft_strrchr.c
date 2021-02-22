f05c70abaae826e40c1dd7be99b9d7561356a766
mde-figu <markusfwirz@gmail.com>
2021-02-22 14:29:13 -0300

    strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 19:28:34 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/19 15:22:00 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/22 14:28:56 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,14 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int			i;
-	const char	*len;
+	int			len;
 
-	len = s;
-	i = ft_strlen(s);
-	s = (s + i);
-	while (*s != *len && c != *s)
-		s = s - 1;
-	if (c == *s)
-		return ((char *)s);
-	return (0);
+	len = ft_strlen(s);
+	while (len >= 0)
+	{
+		if (*(s + len) == (char)c)
+			return ((char *)s + len);
+		len = len + 1;
+	}
+	return (NULL);
 }
