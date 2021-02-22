7d1d2feb352eaca6b8de7403076a81c2da4e5336
mde-figu <markusfwirz@gmail.com>
2021-02-22 13:58:06 -0300

    split de novo

--- ft_split.c
+++ ft_split.c
@@ -6,97 +6,72 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 13:02:52 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/19 19:56:10 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/22 13:57:55 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	int		firstposit(char const *str, char c, int start)
+static size_t	wordlength(char const *s, char c)
 {
-	while (str[start] == c)
-		start++;
-	return (start);
+	size_t	i;
+
+	i = 0;
+	while (s[i] && s[i] !=c)
+		i =i + 1;
+	return (i);
 }
 
-static	int		lastposit(char const *str, char c, int start)
+static size_t	wordcounter(char const *s, char c)
 {
-	while (str[start] != c && str[start] != '\0')
-		start++;
-	return (start);
-}
-
-static	int		nb_of_strs(char const *s, char c)
-{
-	int nb;
-	int checkonce;
-
-	nb = 0;
-	checkonce = 1;
-	while (*s != '\0')
-	{
-		if (*s == c)
-			checkonce = 1;
-		else if (*s != c && checkonce)
-		{
-			nb++;
-			checkonce = 0;
-		}
-		s++;
-	}
-	return (nb);
-}
-
-static	char	**str_null(char **s)
-{
-	char	**str;
+	int		total;
 	int		i;
 
 	i = 0;
-	if (s != NULL)
+	total = 0;
+	while (s[i] && s[i] == c)
+		i = i + 1;
+	while(s[i])
 	{
-		while (s[i])
-		{
-			free(s[i]);
-			i++;
-		}
-		free(s);
-		return (NULL);
+		while (s[i] && s[i] != c)
+			i = i + 1;
+		total = total + 1;
+		while (s[i] && s[i] == c)
+			i = i + 1;
 	}
-	else
-	{
-		if (!(str = (char**)malloc(sizeof(char*))))
-			return (NULL);
-		str[0] = NULL;
-		return (str);
-	}
+	return (total);
+}
+
+static char		*nextword(char const *s, char c)
+{
+	while (*s && *s == c)
+		s =s + 1;
+	return ((char*)s);
 }
 
 char			**ft_split(char const *s, char c)
 {
-	char	**sstr;
-	int		sn;
-	int		i;
-	int		start;
-	int		end;
+	char	**retstr;
+	size_t	i;
+	size_t	words;
 
-	end = 0;
-	i = 0;
-	if (s == NULL)
-		return (str_null(NULL));
-	sn = nb_of_strs(s, c);
-	if (!(sstr = (char**)malloc((sn + 1) * sizeof(char*))))
-		return (NULL);
-	while (i < sn)
+	if (s != NULL)
 	{
-		start = firstposit(s, c, end);
-		end = lastposit(s, c, start);
-		if (!(sstr[i] = (char*)malloc((end - start + 1) * sizeof(char))))
-			str_null(sstr);
-		ft_strlcpy(sstr[i++], (s + start), (end++ - start + 1));
+		words = wordcounter(s, c);
+		if (!(retstr = (char**)malloc(sizeof(char*) * (words + 1))))
+			return (NULL);
+		i = 0;
+		while (i < words)
+		{
+			s = nextword(s, c);
+			final[i] = ft_substr(s, 0, wordlength(s, c));
+			if (!retstr[i])
+				return (NULL);
+			i = i + 1;
+			s += wordlength(s, c)
+		}
+		retstr[words] = NULL;
+		return (retstr);
 	}
-	if (!(sstr[i] = (char*)malloc(sizeof(char))))
-		return (NULL);
-	sstr[i] = NULL;
-	return (sstr);
+	return (NULL);
 }
