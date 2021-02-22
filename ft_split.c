fa0f4e274d5b4660a7c14b2b0d36b90758c02eed
mde-figu <markusfwirz@gmail.com>
2021-02-22 13:59:11 -0300

    split de novo

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 13:02:52 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/22 13:57:55 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/22 13:59:07 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,11 +64,11 @@ char			**ft_split(char const *s, char c)
 		while (i < words)
 		{
 			s = nextword(s, c);
-			final[i] = ft_substr(s, 0, wordlength(s, c));
+			retstr[i] = ft_substr(s, 0, wordlength(s, c));
 			if (!retstr[i])
 				return (NULL);
 			i = i + 1;
-			s += wordlength(s, c)
+			s += wordlength(s, c);
 		}
 		retstr[words] = NULL;
 		return (retstr);
