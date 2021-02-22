e44fb618451d5cf2be8f76320f24c679fe24e4c9
mde-figu <markusfwirz@gmail.com>
2021-02-22 14:13:05 -0300

    trim velho

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,45 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mirkios <mirkios@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 11:34:19 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/20 18:40:55 by mirkios          ###   ########.fr       */
+/*   Updated: 2021/02/22 14:12:53 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static unsigned int	test_start(char const *s1, char const *set)
+char		*ft_strtrim(char const *s1, char const *set)
 {
-	unsigned int		i;
+	size_t i;
 
-	i = 0;
-	while (s1[i] != '\0' && ft_strchr(set, s1[i]))
-	{
-		i++;
-	}
-	return (i);
-}
-
-char				*ft_strtrim(char const *s1, char const *set)
-{
-	unsigned int		k;
-	unsigned int		newlen;
-	unsigned int		newstart;
-
-	k = ft_strlen(s1) - 1;
-	newstart = test_start(s1, set);
-	if (newstart == k + 1)
-	{
-		if (!(ft_calloc(1, sizeof(char))))
-			return (NULL);
-		return (ft_calloc(1, sizeof(char)));
-	}
-	while (k >= newstart && ft_strchr(set, s1[k]))
-	{
-		k--;
-	}
-	newlen = (k + 1) - newstart;
-	return (ft_substr(s1, newstart, newlen));
+	if (!s1 || !set)
+		return (0);
+	while (*s1 && ft_strchr(set, *s1))
+		s1 = s1 + 1;
+	i = ft_strlen(s1);
+	while (i && ft_strchr(set, s1[i]))
+		i = i - 1;
+	return (ft_substr(s1, 0, i + 1));
 }
