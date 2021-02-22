68102ad5571b6d4e03c48a7e0e1ae72d57065f62
mde-figu <markusfwirz@gmail.com>
2021-02-22 14:35:28 -0300

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mde-figu <mde-figu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 13:42:03 by mde-figu          #+#    #+#             */
-/*   Updated: 2021/02/19 15:18:08 by mde-figu         ###   ########.fr       */
+/*   Updated: 2021/02/22 14:35:16 by mde-figu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,26 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*nstr;
 	size_t	i;
-	size_t	j;
-
-	nstr = ((char *)malloc(len + 1));
-	if (s == NULL || nstr == NULL)
-		return (0);
-	i = start;
-	j = 0;
-	while (i < ft_strlen(s) && j < len)
+	
+	if (!s)
+		return (NULL);
+	if ((int)len <= 0 || (int)start < 0 ||
+		(int)start > (int)ft_strlen((char *)s) - 1)
 	{
-		nstr[j] = s[i];
-		j = j + 1;
-		i = i + 1;
+		return (ft_strdup(""));
 	}
-	nstr[j] = '\0';
-	return (nstr);
+	nstr = (char *)ft_calloc(len + 1, sizeof(char));
+	if (!nstr)
+	{
+		return (0);
+	}
+	i = 0;
+	while (i < len && *(s + start) != '\0')
+	{
+		*(nstr + i) = *(s + start);
+		i = i + 1;
+		start = start + 1;
+	}
+	*(nstr + i) = '\0';
+	return (nstr) ;
 }
